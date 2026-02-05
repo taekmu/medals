34ed8a02ddd9105e9fde10b79ef0eccede6a0c54
TAEKMU <yongsacocori@gmail.com>
2026-02-05 16:10:40 +0900

    add widget

--- news/api_server.py
+++ news/api_server.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/", StaticFiles(directory="static", html=True))
 
 app.add_middleware(
     CORSMiddleware,
